162cb37f9dc18b42217823db0dbbba5c84435d51
Tarun Sharma <98592314+sharmatarun32@users.noreply.github.com>
2025-09-05 15:44:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("./src/scripts/sync");
 const route = require("./src/routes");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; #port3000
 
 app.use(express.json());
 app.use("/api", route);
